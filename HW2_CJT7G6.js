6200bc07aa736b38d56e684c683dd0891e4e7338
Chris Thompson <cjt7g6@umsl.edu>
2017-09-13 22:21:10 -0500

    1)added additional blue marker and logic to manuever the marker through the board

--- HW2_CJT7G6.js
+++ HW2_CJT7G6.js
@@ -2,35 +2,23 @@
 /*global alert, jQuery*/
 /*global console, jQuery*/
 
-var row369, col369;
-
+//this function uses two loops to grab the element by ID in jQuery and then append numbered grid cells to the container
 function drawGrid() {
     "use strict";
-    var row, col, numRows, numCols, obj1, obj2;
-
-    //grab the objects to get the rows and cols
-    obj1 = document.getElementById('numRows');
-    obj2 = document.getElementById('numCols');
-
-    //get the number of rows and cols
-    numRows = obj1.options[obj1.selectedIndex].value;
-    numCols = obj2.options[obj2.selectedIndex].value;
-
-    //set the global variables for later use
-    row369 = numRows;
-    col369 = numCols;
+    var row, col, k = 0;
 
     //grab the div container by ID and append a 10X10 grid to it with corresponding point elements
-    for (row = 0; row < numRows; row += 1) {
-        for (col = 0; col < numCols; col += 1) {
+    for (row = 0; row < 10; row += 1) {
+        for (col = 0; col < 10; col += 1) {
             $("#container").append('<div class="grid grid' + row + col + '"></div>');
         }
     }
 
     //size of the "grid" elements 620 / 10 because there is a 10x10 grid and this makes it proportionate
-    $(".grid").height(520 / numRows);
-    $(".grid").width(520 / numCols);
+    $(".grid").width(520 / 10);
+    $(".grid").height(520 / 10);
 }
+
 //random number of steps
 function randomNumSteps() {
     "use strict";
@@ -42,11 +30,19 @@ function randomDirection() {
     "use strict";
     return Math.floor(Math.random() * 4);
 }
+
 //sets the css element of the grid to red fill
-function colorPoint(row, col) {
+function colorRedPoint(row, col) {
     "use strict";
     return $('.grid' + row + col).css('background-color', 'red');
 }
+
+//sets the css element of the grid to blue fill
+function colorBluePoint(row, col) {
+    "use strict";
+    return $('.grid' + row + col).css('background-color', 'blue');
+}
+
 //sets the element of the grid to white fill
 function clearPoint(row, col) {
     "use strict";
@@ -57,28 +53,59 @@ function clearPoint(row, col) {
 function doesCellExist(row, col) {
     "use strict";
 
-    if (row > row369 - 1 || col > col369 - 1 || row < 0 || col < 0) {
+    if (row > 9 || col > 9 || row < 0 || col < 0) {
         return false;
     } else {
         return true;
     }
 }
-function isWinningCell(row, col) {
-    "use strict";
-    if (row === 0 && col === col369 - 1) {
-        return true;
-    } else {
-        return false;
-    }
-}
-//will control the overall function of the game
-function gamePlay(row1, col1, counter1, countdown1) {
-    "use strict";
-    var direction, numSteps, fitty1, fitty2 = 0,
-        row = row1, col = col1, flag, countdown = countdown1, counter = counter1, prevCol, prevRow;
-    direction = randomDirection(); //pick the direction
-    numSteps = randomNumSteps(); //pick the number of steps
 
+//detects the winning cell
+function isRedWinningCell(row, col) {
+    "use strict";
+
+    if ((row === 0 && col === 9)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//detects the winning cell
+function isBlueWinningCell(row, col) {
+    "use strict";
+
+    if ((row === 0 && col === 0)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//detects if the cell is occupied
+function isCellOccupied(redRow, redCol, blueRow, blueCol) {
+    "use strict";
+    if((redRow === blueRow && redCol === blueCol) ||
+        (blueRow === redRow && blueCol === redCol)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+//will control the overall function of the game
+function gamePlay(row1, col1, row2, col2, counter1, counter2, countdown1) {
+    "use strict";
+
+    var redRandomDirection, blueRandomDirection, redNumSteps, blueNumSteps, fitty1, fitty2 = 0,
+        redRow = row1, redCol = col1, flag, countdown = countdown1, redCounter = counter1,
+        blueCounter = counter2, prevCol, prevRow, blueRow = row2, blueCol = col2, cellArray;
+
+    redRandomDirection = randomDirection(); //pick the direction
+    blueRandomDirection = randomDirection(); //pick the direction
+    redNumSteps = randomNumSteps(); //pick the number of steps
+    blueNumSteps = randomNumSteps();
 
     //using setTimeout to show the appearance of animation
     setTimeout(function () {
@@ -87,101 +114,182 @@ function gamePlay(row1, col1, counter1, countdown1) {
         //if the direction is 1, then up
         //if the direction is 2, then right
         //if the direction is 3, then down
-        if (direction === 0) {
-            clearPoint(row, col); //clear the current point
-            prevCol = col; //assign this just in case the cell we're moving to doesn't exist
-            col = col - numSteps; //make the direction adjustment and the number of steps we're moving
 
-            //make a check to see if the cell exists before coloring.
+        /* I put the red stuff first so it executes first. Therefore, red should move first */
+        if (redRandomDirection === 0) {
+            clearPoint(redRow, redCol); //clear the current point
+            prevCol = redCol; //assign this just in case the cell we're moving to doesn't exist
+            redCol = redCol - redNumSteps; //make the direction adjustment and the number of steps we're moving
+
+            //make a check to see if the cell exists and is not occupied before coloring.
             //if it does exist, then color it and mark the stats
             //otherwise, re-color the current point and mark the stats
-            if (doesCellExist(row, col) === true) {
-                colorPoint(row, col);
+            if (doesCellExist(redRow, redCol) === true && isCellOccupied(redRow, redCol, blueRow, blueCol) === false) {
+                colorRedPoint(redRow, redCol);
                 countdown -= 1;
-                counter = counter + 1;
+                redCounter = redCounter + 1;
+
             } else {
-                col = prevCol;
-                colorPoint(row, col);
+                redCol = prevCol;
+                colorRedPoint(redRow, redCol);
                 countdown -= 1;
-                counter = counter + 1;
+                redCounter = redCounter + 1;
             }
-        } else if (direction === 1) {
-            clearPoint(row, col);
-            prevRow = row;
-            row = row + numSteps;
-            if (doesCellExist(row, col) === true) {
-                colorPoint(row, col);
+
+        } else if (redRandomDirection === 1) {
+            clearPoint(redRow, redCol);
+            prevRow = redRow;
+            redRow = redRow + redNumSteps;
+
+            if (doesCellExist(redRow, redCol) === true && isCellOccupied(redRow, redCol, blueRow, blueCol) === false) {
+                colorRedPoint(redRow, redCol);
                 countdown -= 1;
-                counter = counter + 1;
+                redCounter = redCounter + 1;
+
             } else {
-                row = prevRow;
-                colorPoint(row, col);
+                redRow = prevRow;
+                colorRedPoint(redRow, redCol);
                 countdown -= 1;
-                counter = counter + 1;
+                redCounter = redCounter + 1;
             }
-        } else if (direction === 2) {
-            clearPoint(row, col);
-            prevCol = col;
-            col = col + numSteps;
-            if (doesCellExist(row, col) === true) {
-                colorPoint(row, col);
+
+        } else if (redRandomDirection === 2) {
+            clearPoint(redRow, redCol);
+            prevCol = redCol;
+            redCol = redCol + redNumSteps;
+
+            if (doesCellExist(redRow, redCol) === true && isCellOccupied(redRow, redCol, blueRow, blueCol) === false) {
+                colorRedPoint(redRow, redCol);
                 countdown -= 1;
-                counter = counter + 1;
+                redCounter = redCounter + 1;
+
             } else {
-                col = prevCol;
-                colorPoint(row, col);
+                redCol = prevCol;
+                colorRedPoint(redRow, redCol);
                 countdown -= 1;
-                counter = counter + 1;
+                redCounter = redCounter + 1;
             }
+
         } else {
-            clearPoint(row, col);
-            prevRow = row;
-            row = row - numSteps;
-            if (doesCellExist(row, col) === true) {
-                colorPoint(row, col);
+            clearPoint(redRow, redCol);
+            prevRow = redRow;
+            redRow = redRow - redNumSteps;
+
+            if (doesCellExist(redRow, redCol) === true && isCellOccupied(redRow, redCol, blueRow, blueCol) === false) {
+                colorRedPoint(redRow, redCol);
                 countdown -= 1;
-                counter = counter + 1;
+                redCounter = redCounter + 1;
+
             } else {
-                row = prevRow;
-                colorPoint(row, col);
+                redRow = prevRow;
+                colorRedPoint(redRow, redCol);
                 countdown -= 1;
-                counter = counter + 1;
+                redCounter = redCounter + 1;
             }
         }
+
+        if (redRandomDirection === 0) {
+            clearPoint(blueRow, blueCol); //clear the current point
+            prevCol = blueCol; //assign this just in case the cell we're moving to doesn't exist
+            blueCol = blueCol - blueNumSteps; //make the direction adjustment and the number of steps we're moving
+
+            if (doesCellExist(blueRow, blueCol) === true && isCellOccupied(redRow, redCol, blueRow, blueCol) === false) {
+                colorBluePoint(blueRow, blueCol);
+                countdown -= 1;
+                blueCounter = blueCounter + 1;
+
+            } else {
+                blueCol = prevCol;
+                colorBluePoint(blueRow, blueCol);
+                countdown -= 1;
+                blueCounter = blueCounter + 1;
+            }
+
+        } else if (blueRandomDirection === 1) {
+            clearPoint(blueRow, blueCol);
+            prevRow = blueRow;
+            blueRow = blueRow + blueNumSteps;
+
+            if (doesCellExist(blueRow, blueCol) === true && isCellOccupied(redRow, redCol, blueRow, blueCol) === false) {
+                colorBluePoint(blueRow, blueCol);
+                countdown -= 1;
+                blueCounter = blueCounter + 1;
+
+            } else {
+                blueRow = prevRow;
+                colorBluePoint(blueRow, blueCol);
+                countdown -= 1;
+                blueCounter = blueCounter + 1;
+            }
+
+        } else if (blueRandomDirection === 2) {
+            clearPoint(blueRow, blueCol);
+            prevCol = blueCol;
+            blueCol = blueCol + blueNumSteps;
+
+            if (doesCellExist(blueRow, blueCol) === true && isCellOccupied(redRow, redCol, blueRow, blueCol) === false) {
+                colorBluePoint(blueRow, blueCol);
+                countdown -= 1;
+                blueCounter = blueCounter + 1;
+
+            } else {
+                blueCol = prevCol;
+                colorBluePoint(blueRow, blueCol);
+                countdown -= 1;
+                blueCounter = blueCounter + 1;
+            }
+
+        } else {
+            clearPoint(blueRow, blueCol);
+            prevRow = blueRow;
+            blueRow = blueRow - blueNumSteps;
+
+            if (doesCellExist(blueRow, blueCol) === true && isCellOccupied(redRow, redCol, blueRow, blueCol) === false) {
+                colorBluePoint(blueRow, blueCol);
+                countdown -= 1;
+                blueCounter = blueCounter + 1;
+
+            } else {
+                blueRow = prevRow;
+                colorBluePoint(blueRow, blueCol);
+                countdown -= 1;
+                blueCounter = blueCounter + 1;
+            }
+        }
+
 
         //if the countdown is zero, then notify the user we've exhausted all turns and output the counter
         //if the cell is the winning cell, then notify the user that he's awesome and output the counter
         //otherwise, make a recursive call to the function and output the counter
         if (countdown === 0) {
             alert('You have exhausted all turns! Thanks for playing Strange Game!');
-            document.getElementById('boldStuff').innerHTML = counter;
-        } else if (isWinningCell(row, col) === true) {
+            document.getElementById('redBoldStuff').innerHTML = redCounter;
+            document.getElementById('blueBoldStuff').innerHTML = blueCounter;
+
+        } else if (isRedWinningCell(redRow, redCol) === true || isBlueWinningCell(blueRow, blueCol) === true) {
+            document.getElementById('redBoldStuff').innerHTML = redCounter;
+            document.getElementById('blueBoldStuff').innerHTML = blueCounter;
             alert('You Win!!!! Thanks for playing Strange Game!');
-            document.getElementById('boldStuff').innerHTML = counter;
+
         } else {
-            document.getElementById('boldStuff').innerHTML = counter;
-            gamePlay(row, col, counter, countdown);
+            document.getElementById('redBoldStuff').innerHTML = redCounter
+            document.getElementById('blueBoldStuff').innerHTML = blueCounter;;
+            gamePlay(redRow, redCol, blueRow, blueCol, redCounter, blueCounter, countdown); //update the game data and make recursive call
 
             /* Note: I typically don't use recursion, as it's bad for performance. However, this was really the only
-               way to really get the setTimeout function to operate as intended. */
+            way to really get the setTimeout function to operate as intended. */
         }
-    }, 500); //set the setTimeout timer to 500 milliseconds
+
+    }, 200); //set the setTimeout timer to 500 milliseconds
 }
 
 //when the page loads do this
 $(document).ready(function () {
     "use strict";
 
-    var i;
-
-    for (i = 5; i <= 20; i += 1) {
-        $('#numRows').append('<option value="' + i + '">' + i + '</option>');
-    }
-
-
-    for (i = 5; i <= 20; i += 1) {
-        $('#numCols').append('<option value="' + i + '">' + i + '</option>');
-    }
+    drawGrid(); //draw the grid
+    colorRedPoint(9, 0); //starting point for the game
+    colorBluePoint(9, 9);
 });
 
 //when the start button is clicked
@@ -190,11 +298,5 @@ $('#largeButton').click(function () {
 
     //starting the game;
     //row 9, column 0, initialize the counter to zero, and set the countdown to 1,000,000 possible turns
-    gamePlay(row369 - 1, 0, 0, 1000000);
-});
-
-//when the Confirm Selection button is pressed
-$('#largeButton2').click(function () {
-   drawGrid();
-   colorPoint(row369 - 1, 0);
+    gamePlay(9, 0, 9, 9, 0, 0, 1000000);
 });
